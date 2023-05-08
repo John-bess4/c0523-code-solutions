50cc0c18e883299c42265d63f4ded22474778f8f
John-bess4 <jbesss247@gmail.com>
2023-05-08 22:16:10 +0000

    javascript-primitives-and-variables

--- __sample__/main.js
+++ __sample__/main.js
@@ -1,1 +1,22 @@
 console.log('Hello, Code Solutions!');
+const fullName = 'John';
+const numberOfPets = 1000;
+const isCool = true;
+
+console.log('fullName', fullName);
+console.log('numberOfPets', numberOfPets);
+console.log('isCool', isCool);
+
+console.log('type of fullName', typeof fullName);
+console.log('type of numberOfPets', typeof numberOfPets);
+console.log('type of true', typeof isCool);
+
+const unknown = null;
+
+console.log('unknown', unknown);
+console.log('type of unknown', typeof unknown);
+
+let never;
+
+console.log('never', never);
+console.log('type of never', typeof never);
