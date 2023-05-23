311a95145c4e07e4e80aef4c90c416fd980dd81b
John-bess4 <jbesss247@gmail.com>
2023-05-23 22:03:13 +0000

    javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,12 +1,12 @@
-const body = document.body;
-const button = document.querySelector('.colorButton');
+const $body = document.body;
+const $button = document.querySelector('.colorButton');
 
-button.addEventListener('click', function (event) {
-  if (body.className === 'white-bg') {
-    button.className = '';
-    body.className = 'colorButton';
+$button.addEventListener('click', function (event) {
+  if ($body.className === 'white-bg') {
+    $button.className = '';
+    $body.className = 'colorButton';
   } else {
-    body.classList = 'white-bg';
-    button.classList = 'yellow-bg';
+    $body.classList = 'white-bg';
+    $button.classList = 'yellow-bg';
   }
 });
