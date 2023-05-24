c9e4d043ff000dd700124cc17db88ccad1020461
John-bess4 <jbesss247@gmail.com>
2023-05-24 17:59:19 +0000

    javascript-forms

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,14 @@
+const contactForm = document.querySelector('#contact-form');
+
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = {
+    name: contactForm.elements.name.value,
+    email: contactForm.elements.email.value,
+    message: contactForm.elements.message.value,
+  };
+  console.log('contactForm:', contactForm);
+  console.log('formData:', formData);
+  contactForm.reset();
+});
