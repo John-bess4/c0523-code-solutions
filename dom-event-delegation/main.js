ecf72ecbb2887c77f11721e9b04ef783140220fc
John-bess4 <jbesss247@gmail.com>
2023-05-25 21:15:12 +0000

    event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,11 @@
+const taskList = document.querySelector('.task-list');
+taskList.addEventListener('click', function (event) {
+  console.log('Event Target:', event.target);
+  console.log('Tag Name:', event.target.tagName);
+
+  if (event.target.tagName === 'BUTTON') {
+    const taskListItem = event.target.closest('.task-list-item');
+    console.log('Task List Item:', taskListItem);
+    taskListItem.remove();
+  }
+});
